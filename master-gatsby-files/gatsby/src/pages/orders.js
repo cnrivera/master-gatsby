bfb5d97978c33980d5de69c7c73d5e9f1f909387
Noel Rivera <christina.rivera@kroger.com>
2020-12-09 14:09:50 -0500

    add all files

---
+++ master-gatsby-files/gatsby/src/pages/orders.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function OrdersPage() {
+  return (
+    <div>
+      <p>Hey! I've the orders page.</p>
+    </div>
+  );
+}
---
+++ master-gatsby-files/gatsby/src/pages/pizzas.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function PizzasPage() {
+  return (
+    <div>
+      <p>Hey! I've the pizzas page.</p>
+    </div>
+  );
+}
